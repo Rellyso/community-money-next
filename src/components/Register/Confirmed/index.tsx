b3680e57f2c6e2b99ab31377d83d1d87eac4dc60
taisso <taissoreni@gmail.com>
2021-10-16 13:44:20 -0300

    crate component of register confirmed

--- src/components/Register/Confirmed/index.tsx
+++ src/components/Register/Confirmed/index.tsx
@@ -4,11 +4,21 @@ import style from "./style.module.scss";
 const Confirmed: NextPage = () => {
   return (
     <div className={style.container}>
-      <span>Logo</span>
-      <p>Parabéns! Você acaba de criar sua conta!</p>
-      <img src="../../img/clapping.svg" alt="Batida de palmas" />
-      <p>Explorar o Community Money!</p>
-      <img src="../../img/arrow-left.svg" alt="Flecha direcionada a esquerda" />
+      <div className={style.content}>
+        <img src="../../img/logo2.svg" alt="Segunda logo" draggable="false" />
+        <p>Parabéns! Você acaba de criar sua conta!</p>
+        <img
+          src="../../img/clapping.svg"
+          alt="Batida de palmas"
+          draggable="false"
+        />
+        <p>Explorar o Community Money!</p>
+        <img
+          src="../../img/arrow-left.svg"
+          alt="Flecha direcionada a esquerda"
+          draggable="false"
+        />
+      </div>
     </div>
   );
 };
