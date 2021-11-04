cb89b38e7caf399716bab57d427b9e3c8252ad57
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-11-03 22:41:06 -0300

    created homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import { Hero } from "components/Hero";
 import type { NextPage } from "next";
+import { useState } from "react";
+
+import styles from './styles/Home.module.scss';
 
 const Home: NextPage = () => {
-  return <>LandingPage</>;
+  return (
+    <div className={styles.container}>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
